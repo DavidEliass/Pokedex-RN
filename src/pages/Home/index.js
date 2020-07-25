951ec93ba5372da8ede62e6c639ee56a3a9a49f2
CenourinhaBR <jogadorbr602@gmail.com>
2020-07-25 12:12:23 -0300

    Adicionado a estruras dos Cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,11 +9,18 @@ import Generation from '../../assets/icons/generation'
 import Sort from '../../assets/icons/sort'
 import Filter from '../../assets/icons/filter'
 
+//Type 
+import Glass_Type from '../../assets/types/glass'
+import Poison_Type from '../../assets/types/poison'
+
+//Pokemon Png
+
+import Bulbasaur from '../../assets/generations/generation1/001.png'
+
 // Import Style
 import styles from './style';
 
 //Import Component
-
 import Search from '../../components/Search'
 
 
@@ -26,28 +33,43 @@ export default function Home() {
   const onChangeSearch = query => setSearchQuery(query);
 
   return (
-  <View style={styles.Container}>
-     <Pokeball width="75" height="75" style={styles.patterns} />
-    <View style={styles.TabIcon}>
-      <TouchableOpacity><Generation/></TouchableOpacity>
-      <TouchableOpacity><Sort /></TouchableOpacity>
-      <TouchableOpacity><Filter /></TouchableOpacity>
-      
-      
-    </View>
-    <View>
-      <Text style={styles.Title}>Pokédex</Text>
-      <Text style={styles.SubTitle}>Search for Pokémon by name or using the National Pokédex number.</Text> 
-      <Search />
+    <View style={styles.Container}>
+      <Pokeball width="75" height="75" style={styles.patterns} />
+      <View style={styles.TabIcon}>
+        <TouchableOpacity><Generation /></TouchableOpacity>
+        <TouchableOpacity><Sort /></TouchableOpacity>
+        <TouchableOpacity><Filter /></TouchableOpacity>
+
+
+      </View>
+      <View>
+        <Text style={styles.Title}>Pokédex</Text>
+        <Text style={styles.SubTitle}>Search for Pokémon by name or using the National Pokédex number.</Text>
+        <Search />
+      </View>
+
+      <View style={styles.CardList}>
+        <View style={styles.Cards}>
+        
+          <Text style={styles.number}>#001</Text>
+          <Text style={styles.pokemonName}>Bulbasaur</Text>
+          <View style={styles.typeGroup}>
+            <View style={styles.typeTag}>
+            <Glass_Type  style={styles.badgtes}/><Text style={styles.typeText}>Grass</Text>
+            </View>
+
+            <View style={styles.typeTag2}>
+            <Poison_Type  style={styles.badgtes}/><Text style={styles.typeText}>Poison</Text>
+            </View>
+            
+
+            
+          </View>
+        </View>
+      </View>
     </View>
 
-    <View>
-      
-    </View>
 
-  </View> 
-    
-
-    );
+  );
 }
 
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -30,7 +30,80 @@ export default StyleSheet.create({
         fontSize: 16,
         color: '#747476',
         fontFamily: 'sf-pro-display-regular'
+    },
+
+    Cards: {
+        borderRadius: 10,
+        width: 325,
+        height: 144,
+        backgroundColor: '#8BBE8A',
+        paddingLeft: 20,
+        paddingTop: 20
+    },
+
+    CardList: {
+        paddingTop: 45,
+
+    },
+
+    number: {
+        fontSize: 12,
+        fontFamily: 'sf-pro-display-bold',
+        color: 'rgba(23, 23, 27, 0.6);'
+    },
+
+    pokemonName: {
+        fontSize: 26,
+        fontFamily: 'sf-pro-display-bold',
+        color: '#FFFFFF'
+    },
+
+    typeGroup: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 5,
+        
+    },
+
+    typeTag: {
+        backgroundColor: '#62B95790',
+        width: 80,
+        height: 50,
+        alignItems: 'center',
+        paddingTop: 3,
+        borderRadius: 3,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        marginRight: 10
+        
+    },
+
+    typeTag2: {
+        backgroundColor: '#A552CC',
+        width: 80,
+        height: 50,
+        alignItems: 'center',
+        paddingTop: 3,
+        borderRadius: 3,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        marginRight: 10
+        
+    },
+
+    typeText: {
+        fontSize: 12,
+        color: '#FFFFFF',
+        fontFamily: 'sf-pro-display-bold',
+        paddingLeft: -10,
+    },
+
+    badgtes: {
+        
     }
+
+
 })
 
 
